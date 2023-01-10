d6718dec9660f88d27d360a28aa0576b68646841
Babatunde Adeoti <babaolu98@gmail.com>
2023-01-10 20:07:07 +0100

    Added debug messages capabilities by Validation layers

--- VFirst.cpp
+++ VFirst.cpp
@@ -23,6 +23,26 @@ const bool enableValidationLayers = false;
 const bool enableValidationLayers = true;
 #endif
 
+// Proxy function to vkCreateDebugUtilsMessengerEXT
+    VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo, const VkAllocationCallbacks *pAllocator, VkDebugUtilsMessengerEXT *pDebugMessenger)
+    {
+      auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+      
+      if (func != nullptr)
+	return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+      else
+	return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+
+//
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks *pAllocator)
+{
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+
+  if (func != nullptr)
+    func(instance, debugMessenger, pAllocator);
+}
+
 class HelloTriangleApplication
 {
 	public:
@@ -37,6 +57,8 @@ class HelloTriangleApplication
 	private:
 		GLFWwindow *window;
 		VkInstance instance;
+  VkDebugUtilsMessengerEXT debugMessenger;
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
 		void initWindow()
 		{
@@ -50,7 +72,31 @@ class HelloTriangleApplication
 		void initVulkan()
 		{
 			createInstance();
+			setupDebugMessenger();
+			pickPhysicalDevice();
 		}
+
+  void pickPhysicalDevice()
+  {
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+    if (deviceCount == 0)
+      throw std::runtime_error("Failed to find GPUs with Vulkan support!");
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    for (const auto& device : devices)
+      if (isDeviceSuitable(device))
+	{
+	  physicalDevice = device;
+	  break;
+	}
+
+    if (physicalDevice == VK_NULL_HANDLE)
+      throw std::runtime_error("Failed to find a suitable GPU!");
+  }
 
 		void createInstance()
 		{
@@ -69,16 +115,23 @@ class HelloTriangleApplication
 			createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 			createInfo.pApplicationInfo = &appInfo;
 
+		       	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+			
 			if (enableValidationLayers)
 			  {
 			    std::cout << "Validation Layer enabled!" << std::endl;
 
 			    createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
 			    createInfo.ppEnabledLayerNames = validationLayers.data();
+
+			    populateDebugMessengerCreateInfo(debugCreateInfo);
+			    createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT *) &debugCreateInfo;
 			  }
 			else
 			  {
 			    createInfo.enabledLayerCount = 0;
+
+			    createInfo.pNext = nullptr;
 			  }
 
 			auto glfwExtensions = getRequiredExtensions();
@@ -154,6 +207,46 @@ class HelloTriangleApplication
 
     return true;
   }
+
+  // Message callback function
+  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+						      VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+						      VkDebugUtilsMessageTypeFlagsEXT messageType,
+						      const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+						      void *pUserData)
+  {
+    std::cerr << "Validation layer: " << pCallbackData->pMessage << std::endl;
+
+    return VK_FALSE;
+  }
+
+  void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo)
+  {
+    createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+      VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = debugCallback;
+    createInfo.pUserData = nullptr;// Optional
+
+  }
+  
+  void setupDebugMessenger()
+  {
+    if (!enableValidationLayers) return;
+
+    VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+
+    populateDebugMessengerCreateInfo(createInfo);
+    
+    if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+      throw std::runtime_error("Failed to set up debug messenger!");
+  }
+  
 		void mainLoop()
 		{
 			while (!glfwWindowShouldClose(window))
@@ -162,10 +255,14 @@ class HelloTriangleApplication
 
 		void cleanup()
 		{
+		  if (enableValidationLayers)
+		   DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+		  
 		  vkDestroyInstance(instance, nullptr);
-			glfwDestroyWindow(window);
 
-			glfwTerminate();
+		  glfwDestroyWindow(window);
+
+		  glfwTerminate();
 		}
 };
 
